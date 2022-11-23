6a8dc50b781a38f3cd4e83a14471d913e14fbc43
katiacardoso <kt.cardosos@gmail.com>
2022-11-23 13:28:47 -0400

    inital commit

---
+++ app_flutter/lib/cadastro_mudas.dart
@@ -0,0 +1,85 @@
+import 'package:app_flutter/model/user_plantas.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(CadastrarMudas());
+
+class CadastrarMudas extends StatelessWidget {
+  // This widget is the root of your application.
+
+  final TextEditingController _controladorNomeComum = TextEditingController();
+  final TextEditingController _controladorNomeCientifico =
+      TextEditingController();
+  final TextEditingController _controladorQuantidade = TextEditingController();
+  final TextEditingController _controladorData = TextEditingController();
+  final TextEditingController _controladorNomeDeterminador =
+      TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Cadastrando mudas'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: TextField(
+                  controller: _controladorNomeComum,
+                  decoration: InputDecoration(labelText: 'Nome Comum:'),
+                  keyboardType: TextInputType.text),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: TextField(
+                  controller: _controladorNomeCientifico,
+                  decoration: InputDecoration(labelText: 'Nome Cienfico:'),
+                  keyboardType: TextInputType.text),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: TextField(
+                  controller: _controladorQuantidade,
+                  decoration:
+                      InputDecoration(labelText: 'Quantidade de mudas:'),
+                  keyboardType: TextInputType.number),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: TextField(
+                  controller: _controladorData,
+                  decoration: InputDecoration(labelText: 'Data da plantação:'),
+                  keyboardType: TextInputType.datetime),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: TextField(
+                  controller: _controladorNomeDeterminador,
+                  decoration: InputDecoration(labelText: 'Nome Determinador:'),
+                  keyboardType: TextInputType.text),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: ElevatedButton(
+                child: Text('Cadastrar'),
+                onPressed: () {
+                  final String nomeComum = _controladorNomeComum.text;
+                  final String nomeCientifico = _controladorNomeCientifico.text;
+                  final int quantidade = int.parse(_controladorQuantidade.text);
+                  final String data = _controladorData.text;
+                  final String nomeDeterminador =
+                      _controladorNomeDeterminador.text;
+
+                  final Plantas mudaNova = Plantas(nomeComum, nomeCientifico,
+                      quantidade, data, nomeDeterminador);
+                  print(mudaNova);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ app_flutter/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:app_flutter/cadastro_mudas.dart';
+import 'package:app_flutter/login/login_screen.dart';
+//import 'package:bnb_flutter/login/login_screen.dart';
+//import 'package:bnb_flutter/login/signup_screen.dart';
+//import 'package:bnb_flutter/units/unit_screen.dart';
+//import 'package:bnb_flutter/utils/first_page.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+//import 'screen/home_screen.dart';
+//import 'units/unit_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); //vinculação da aplicação com o firebase
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: "AIzaSyCAfFtbWKBivwantM7EG1TIo4JjtuTSqdM",
+    appId: "AIzaSyCAfFtbWKBivwantM7EG1TIo4JjtuTSqdM",
+    messagingSenderId: "602721387882",
+    projectId: "irrigacao-7f9a1",
+  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      initialRoute: '/home', //primeira página a ser chamada
+      routes: {
+        '/home': (context) => LoginScreen()
+        //'/home': (context) => const HomeScreen(),
+        //'/unit': (context) => UnitScreen(),
+        //'/login': (context) => LoginScreen(),
+        //'/signUp':(context) => SignUpScreen(),
+      },
+    );
+  }
+}
